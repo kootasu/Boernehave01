ef4ab4dd97b30da9cf1658f43d3c05740a935e17
kootasu <kootasu@hotmail.com>
2020-03-23 09:24:45 +0100

    Tester Update project igen

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Børnehave!");
-        System.out.println("Tester lige, hvordan Update project fungerer");
-        System.out.println("Jeg lægger disse linjer tekst op, sletter dem i Intellij og opdaterer");
+
     }
 }
---
+++ src/UpdateTestKlasse.java
@@ -0,0 +1,5 @@
+public class UpdateTestKlasse {
+
+    // Hey hey hey, jeg afprøver lige Update project-knappen (den blå pil)
+
+}
