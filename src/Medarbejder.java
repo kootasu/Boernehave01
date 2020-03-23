1233899aae7727fef11654e035dbd8c74644bb3f
Thor7578 <56076721+Thor7578@users.noreply.github.com>
2020-03-23 14:17:19 +0100

    Medarbejder

---
+++ src/Medarbejder.java
@@ -0,0 +1,33 @@
+public class Medarbejder {
+    private String navn;
+    private String email;
+    private String telefonnummer;
+    private String brugernavn;
+    private String password;
+    private String stue;
+    private String stilling;
+
+
+
+    public Medarbejder(String navn, String email, String telefonnummer, String brugernavn, String password, String stue, String stilling) {
+        this.navn = navn;
+        this.email = email;
+        this.telefonnummer = telefonnummer;
+        this.brugernavn = brugernavn;
+        this.password = password;
+        this.stue = stue;
+        this.stilling = stilling;
+    }
+
+    public void seVagtplan() {     //Metode
+        
+    }
+
+    private void sletVagtOenske() { //Metode
+
+    }
+
+    private void oenskVagt() {      //Metode
+        
+    }
+}
