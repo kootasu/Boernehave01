54c5a6723c692afce48eec8e35415379cb89f070
kootasu <kootasu@hotmail.com>
2020-03-23 09:12:59 +0100

    Tester Update project

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Børnehave!");
+        System.out.println("Tester lige, hvordan Update project fungerer");
+        System.out.println("Jeg lægger disse linjer tekst op, sletter dem i Intellij og opdaterer");
     }
 }
